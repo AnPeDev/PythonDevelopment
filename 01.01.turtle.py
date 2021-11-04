fae4dfbe40e23c2fe087f063a04ca9b5730f6abd
AnPeDev <AnPeDev@gmail.com>
2021-11-04 20:50:23 +0400

    Turtle star

---
+++ 01.01.turtle.py
@@ -0,0 +1,20 @@
+import turtle as tur
+
+
+def star():
+    tur.color('red', 'yellow')
+    tur.speed(10)
+    tur.begin_fill()
+    for step in range(5):
+        tur.forward(70)
+        tur.left(144)
+    tur.end_fill()
+
+
+for starStep in range(5):
+    star()
+    tur.forward(100)
+    tur.left(360 / 5)
+
+
+tur.done()
