a2ec466cd12550492836964580d03029f3523fae
AnPeDev <AnPeDev@gmail.com>
2021-11-10 18:52:20 +0400

    lesson 4, structural programming

---
+++ PythonTraining/04_doc_string, main().py
@@ -0,0 +1,22 @@
+# def paint_house(x, y, width, height):
+#     '''Возвращает ничего или пасс'''
+#     # pass
+#     return None
+
+
+# paint_house(10, 20, 30, 40)
+
+def main():
+    paint_house(10, 20, 30, 40)
+
+
+def paint_house(x, y, width, height):
+    '''Возвращает ничего или пасс'''
+    # pass
+    return None
+
+
+main()
+
+print(paint_house.__doc__)
+# Выводит документ строки
