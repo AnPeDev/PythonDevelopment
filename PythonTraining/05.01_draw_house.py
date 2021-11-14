8d5a5eebaf70d6d4207ac02928b3ba440570bcbc
AnPeDev <75500740+AnPeDev@users.noreply.github.com>
2021-11-14 18:22:53 +0400

    Draw house

--- PythonTraining/05.01_draw_house.py
+++ PythonTraining/05.01_draw_house.py
@@ -1,3 +1,9 @@
+def main():
+    x, y = 300, 400
+    width, height = 200, 300
+    draw_house(x, y, width, height)
+
+
 def draw_house(x, y, width, height):
     """
     Нарисовать домик широной width и высотой height, с опорной точки x и y,
@@ -6,10 +12,40 @@ def draw_house(x, y, width, height):
     width, height: полная ширина и высота домика
     return: None
     """
-    print('Типа рисую домик...', x, y, width, height)
+    print('Рисую домик...', x, y, width, height)
+    foundation_height = 0.05 * height
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, (y - walls_height - foundation_height),
+                    width, roof_height)
+
+
+def draw_house_foundation(x, y, width, height):
+    """
+    Рисует основание домика
+    """
+    print('Рисую основание домика...', x, y, width, height)
     pass
 
 
-x, y = 100, 100
-width, height = 200, 200
-draw_house(x, y, width, height)
+def draw_house_walls(x, y, width, height):
+    """
+    Рисует стены домика
+    """
+    print('Рисую стены домика...', x, y, width, height)
+    pass
+
+
+def draw_house_roof(x, y, width, height):
+    """
+    Рисует крышу домика
+    """
+    print('Рисую крышу домика...', x, y, width, height)
+    pass
+
+
+main()
