2fe4b7d56e2cbb15c2446ec38683064329997cc1
AnPeDev <AnPeDev@gmail.com>
2021-11-14 12:23:23 +0400

    Draw house

---
+++ PythonTraining/05.01_draw_house.py
@@ -0,0 +1,15 @@
+def draw_house(x, y, width, height):
+    """
+    Нарисовать домик широной width и высотой height, с опорной точки x и y,
+    которая находитс в нижней точке фундамента
+    x, y: координаты опорной точки, середины домика
+    width, height: полная ширина и высота домика
+    return: None
+    """
+    print('Типа рисую домик...', x, y, width, height)
+    pass
+
+
+x, y = 100, 100
+width, height = 200, 200
+draw_house(x, y, width, height)
